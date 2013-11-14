377ad441f455026763b919957ddc70cca55fec7c
Trevor Reed <trevorar@usc.edu>
2013-11-13 23:47:47 -0800

    Created MarketOrder and CityMap classes

---
+++ src/city/CityMap.java
@@ -0,0 +1,11 @@
+package city;
+
+import java.util.*;
+
+public class CityMap {
+	//Map of bus stop numbers to nearby destinations. Necessary?
+	//Map<int, List<String>> busStops = new ArrayMap<int, List<String>>();
+
+	//We may just keep a list of Restaurants, Banks, Markets, and BusStops here instead.
+	//Depends how the gui ends up working...
+}
---
+++ src/city/MarketOrder.java
@@ -0,0 +1,13 @@
+package city;
+
+import java.util.*;
+
+public class MarketOrder {
+	
+	List<OrderItem> orders = new ArrayList<OrderItem>();
+	
+	String destination;
+	
+	PersonAgent recipient; //Should usually be a CookRole - may need to change later
+
+}
