f584d2cc20ec9622cecdcf29db1b13b8d2045276
grant.derderian@usc.edu <gderderi@usc.edu>
2013-11-17 19:03:46 -0800

    Adding in base skeleton code for market objects

---
+++ src/city/Market.java
@@ -0,0 +1,18 @@
+/*
+	Market Class - Supports MarketWorker role and MarketManager role
+*/
+package city;
+
+import java.util.*;
+
+// Class modeled after https://github.com/usc-csci201-fall2013/simcity201/issues/28
+
+public class Market {
+
+	Location location;
+	Host host;
+	string customerRole; //value is something like “Restaurant1CustomerRole”
+	Menu m;
+	String type;
+
+}
