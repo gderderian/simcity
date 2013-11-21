9edee951d1a1b12a78600e042c1a59ea62789b3e
grant.derderian@usc.edu <gderderi@usc.edu>
2013-11-21 00:06:13 -0800

    Adding in tested compoments of marketManager

---
+++ src/Role/MarketWorker.java
@@ -0,0 +1,20 @@
+package Role;
+
+import java.util.*;
+
+import city.MarketOrder;
+import city.PersonAgent;
+import city.transportation.Vehicle;
+import Role.Role;
+
+public class MarketWorker extends Role {
+	
+	// Data
+	int numWorkingOrders;
+	
+	// Messages
+	public void msgPrepareOrder(MarketOrder o){
+		
+	}
+	
+}
