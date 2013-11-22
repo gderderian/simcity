88fee8a2ba1504712ab807bf8779525a35f769b4
Trevor Reed <trevorar@usc.edu>
2013-11-21 17:25:15 -0800

    Updated bus stop and finished bus stop test. close #4

--- src/city/transportation/BusStopAgent.java
+++ src/city/transportation/BusStopAgent.java
@@ -9,9 +9,9 @@ import agent.Agent;
 
 public class BusStopAgent extends Agent implements BusStop {
 	//Data
-	List<PersonAgent> peopleWaiting = new ArrayList<PersonAgent>();
+	public List<PersonAgent> peopleWaiting = new ArrayList<PersonAgent>();
 	
-	List<MyBus> buses = new ArrayList<MyBus>();
+	public List<MyBus> buses = new ArrayList<MyBus>();
 	
 	class MyBus {
 		BusAgent b;
@@ -35,7 +35,7 @@ public class BusStopAgent extends Agent implements BusStop {
 	
 	
 	//Scheduler
-	protected boolean pickAndExecuteAnAction() {
+	public boolean pickAndExecuteAnAction() {
 		if(!buses.isEmpty()) {
 			sendPassengersToBus(buses.get(0));
 			return true;
@@ -46,6 +46,13 @@ public class BusStopAgent extends Agent implements BusStop {
 	
 	//Actions
 	private void sendPassengersToBus(MyBus b) {
+		if(peopleWaiting.isEmpty()) {
+			print("Sorry, no passengers waiting for bus!");
+			b.b.msgPeopleBoarding(null);
+			buses.remove(b);
+			return;
+		}
+		
 		List<PersonAgent> newPassengers = new ArrayList<PersonAgent>();
 		int i = 0;
 		while(i < b.openSpots && !peopleWaiting.isEmpty()) {
@@ -56,6 +63,7 @@ public class BusStopAgent extends Agent implements BusStop {
 			i++;
 		}
 		
+		print("Here are " + newPassengers.size() + " passengers!");
 		b.b.msgPeopleBoarding(newPassengers);
 		buses.remove(b);
 	}
