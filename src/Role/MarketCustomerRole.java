3f1d3b949acc5257c181457f2e951acc3885b199
grant.derderian@usc.edu <gderderi@usc.edu>
2013-11-23 11:16:38 -0800

    Adding in new MarketCustomerRole skeleton file as part of issue #7

---
+++ src/Role/MarketCustomerRole.java
@@ -0,0 +1,19 @@
+package Role;
+
+import Role.Role;
+
+public class MarketCustomerRole extends Role {
+
+	// Data
+	
+	// Messages
+	
+	// Scheduler
+	public boolean pickAndExecuteAnAction() {
+		return false;
+	}
+	
+	// Actions
+	
+
+}
