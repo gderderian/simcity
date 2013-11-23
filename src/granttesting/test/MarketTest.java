c3769fae4a99b7e722b48ceed5c30763b1d329ef
grant.derderian@usc.edu <gderderi@usc.edu>
2013-11-23 11:43:21 -0800

    Adding new market test files as part of #43

---
+++ src/granttesting/test/MarketTest.java
@@ -0,0 +1,12 @@
+package granttesting.test;
+
+import junit.framework.TestCase;
+import hollytesting.test.mock.MockBankTellerRole;
+import hollytesting.test.mock.MockHouse;
+import city.PersonAgent;
+
+public class MarketTest extends TestCase{
+        
+        
+
+}
