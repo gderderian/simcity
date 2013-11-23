1bf5dd188c8f9bd54433d7506a547adea63e1f72
grant.derderian@usc.edu <gderderi@usc.edu>
2013-11-22 21:46:52 -0800

    Making initial draft of citytimer

--- src/city/gui/CityClock.java
+++ src/city/gui/CityClock.java
@@ -2,25 +2,30 @@ package city.gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Timer;
-import java.util.TimerTask;
+import javax.swing.Timer;
 
 public class CityClock implements ActionListener {
+	
 	CityGui cityGui;
-	Timer time= new Timer();
+	Timer cityTime;
 	
 	public CityClock(CityGui cityGui){
 		this.cityGui= cityGui;
 	}
 
-	public startTime(){
-		time.scheduleAtFixedRate(new TimerTask() {
-			@Override public void run() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					
-				}
-			}}, 179999);
+	public void startTime(){
+		
+		cityTime = new Timer(179999,
+				new ActionListener() { public void actionPerformed(ActionEvent event) {
+					// Timer is done, it is now a new day
+		      }
+		});
+		cityTime.start();
+		
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		
 	}
 	
 	
