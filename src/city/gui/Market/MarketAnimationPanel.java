979be0fe2f439d2c681c13adcb8ac558ca6503e8
Grant Derderian <gderderi@usc.edu>
2013-11-25 15:47:16 -0800

    Fixing bug accidentally introduced by removal of back to city gui button

--- src/city/gui/Market/MarketAnimationPanel.java
+++ src/city/gui/Market/MarketAnimationPanel.java
@@ -118,5 +118,11 @@ public class MarketAnimationPanel extends BuildingPanel implements ActionListene
 		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
     
 }
