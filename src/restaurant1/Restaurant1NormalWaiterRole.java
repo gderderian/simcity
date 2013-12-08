a9637a9e1ecc81ef964dc75b9b1b8ecb27c1c19d
Trevor Reed <trevorar@usc.edu>
2013-12-08 14:49:06 -0800

    Switched normal waiter code from shared data waiter to normal waiter

--- src/restaurant1/Restaurant1NormalWaiterRole.java
+++ src/restaurant1/Restaurant1NormalWaiterRole.java
@@ -1,5 +1,6 @@
 package restaurant1;
 
+import restaurant1.Restaurant1WaiterRole.customerState;
 import city.PersonAgent;
 
 public class Restaurant1NormalWaiterRole extends Restaurant1WaiterRole {
@@ -8,9 +9,11 @@ public class Restaurant1NormalWaiterRole extends Restaurant1WaiterRole {
 		super(name, p);
 	}
 
-	@Override
 	void sendOrderToCook(MyCustomer c) {
+		log("Sending " + c.c.getName() + "'s order of " + c.choice + " to cook wirelessly. Isn't technology great?");
 		
+		c.s = customerState.orderSentToCook;
+		cook.msgHereIsOrder(this, c.choice, c.table);	
 	}
 
 }
--- src/restaurant1/Restaurant1SharedDataWaiterRole.java
+++ src/restaurant1/Restaurant1SharedDataWaiterRole.java
@@ -8,11 +8,7 @@ public class Restaurant1SharedDataWaiterRole extends Restaurant1WaiterRole {
 		super(name, p);
 	}
 
-	void sendOrderToCook(MyCustomer c) {
-		log("Sending " + c.c.getName() + "'s order of " + c.choice + " to cook wirelessly. Isn't technology great?");
-		
-		c.s = customerState.orderSentToCook;
-		cook.msgHereIsOrder(this, c.choice, c.table);		
+	void sendOrderToCook(MyCustomer c) {	
 	}
 
 }
