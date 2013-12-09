37f33f8b0422055f3b711224560047768a4e2653
Tom <teryun93@gmail.com>
2013-12-08 16:25:07 -0800

    adding new rest5

--- src/city/Restaurant5/Restaurant5WaiterRole.java
+++ src/city/Restaurant5/Restaurant5WaiterRole.java
@@ -541,8 +541,34 @@ public class Restaurant5WaiterRole extends Role implements Restaurant5Waiter {
 
 
 	}
+	
+	
+	/*
+	Dogotokitchen();
+		try {
+			atKitchen.acquire();
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		cook.msgReceviedOrderFromWaiter(this, customer.choice, customer.table);
 
 
+		Dogobacktolobby();
+		atlobbycurrently=false;
+		//log("NUMBER OF PERMITS = " + atLobby.availablePermits());
+		try {
+			atLobby.acquire();
+			//atLobby.acquire();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		atlobbycurrently = true;
+
+	
+	*/
 	private void GoBackAfterCustomerGotCheck()
 	{
 		Dogobacktolobby();
