3fdf37779a097e2a076ed86f6b3eb97256b75357
Grant Derderian <gderderi@usc.edu>
2013-12-09 19:26:47 -0800

    Fixing private/protected error for MarketMgr trucks

--- src/city/Market.java
+++ src/city/Market.java
@@ -13,7 +13,7 @@ import Role.MarketWorker;
 public class Market { // Class modeled after comment made in https://github.com/usc-csci201-fall2013/simcity201/issues/28
 
 	public MarketManager mktManager;
-	TruckAgent myTruck;
+	public TruckAgent myTruck;
 	String marketName;
 	
 	public Market(){
